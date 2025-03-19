b45aa7ce4331401d72dd10eb3222faaba648e26e
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-03-19 19:59:39 +0300

    made chnges

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+    const formInput = document.querySelector('#new-task-description');
+    const taskList = document.querySelector('#tasks');
+
+    form.addEventListener("submit",(e)=>{
+      e.preventDefault();
+      
+      formInput.textContent="Wash the dishes";
+      
+      const myTasks = document.createElement("li")
+      myTasks.textContent="Wash the dishes";
+
+      taskList.appendChild(myTasks)
+    })
 });
+
+
